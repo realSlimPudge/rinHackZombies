419555393c1246422b9543e508e8a2ed3cd69f48
Filipp <imnomakj@mail.ru>
2024-11-30 09:54:34 +0300

    testing

---
+++ server/test.js
@@ -0,0 +1,7 @@
+await fetch('https://hygieia.fast-report.com/api/rp/v1/Templates/Root', {
+    method: 'GET',
+    headers: {
+      'Authorization': 'Basic YXBpa2V5OnFrZXAzNXJwc2Y0NHNid2N5engzdXI0OGVodXc1YWNuYTN0YmI1aDlkaWExYzZ0ZzRyeG8=',
+      "accept": "text/plain"
+    }
+  });
