ae8f749277058f2376b320950f17ac828b17aa1e
realSlimPudge <komissarov010806@gmail.com>
2024-12-01 09:41:21 +0300

    add Blob

--- frontend/src/Widgets/Functions/useSendImages.tsx
+++ frontend/src/Widgets/Functions/useSendImages.tsx
@@ -6,12 +6,26 @@ const useSendImages = () => {
 
 	const sendImagesToServer = async () => {
 		try {
+			const formData = new FormData()
+
+			images.forEach((image, index) => {
+				const byteCharacters = atob(image.split(',')[1])
+				const byteNumbers = new Array(byteCharacters.length)
+				for (let i = 0; i < byteCharacters.length; i++) {
+					byteNumbers[i] = byteCharacters.charCodeAt(i)
+				}
+				const byteArray = new Uint8Array(byteNumbers)
+				const blob = new Blob([byteArray], { type: 'image/png' })
+				formData.append(`image${index}`, blob)
+			})
+
 			const response = await fetch('http://localhost:3000/images', {
 				method: 'POST',
 				headers: {
 					'Content-Type': 'application/json',
 				},
-				body: JSON.stringify(images),
+				// body: JSON.stringify(images),
+				body: formData,
 			})
 			if (response.ok) {
 				console.log('Images sent successfully')
@@ -19,7 +33,7 @@ const useSendImages = () => {
 				console.error('Failed to send images')
 			}
 		} catch (error) {
-			console.error('Error sending settings:', error)
+			console.error('Error sending images:', error)
 		}
 	}
 
